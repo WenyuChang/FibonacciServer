ee1e8ba42bb9991bb3262011867f81d8825e738a
wenyu <changwy_1987@hotmail.com>
2016-07-15 17:42:05 +0800

    Add gitignore file and code refactor

--- setup.py
+++ setup.py
@@ -19,12 +19,20 @@ class Installer(install):
     """
 
     def _create_bin(self):
-        source_bin_config = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fibonacci', 'templates', 'fibstarter')
-        target_bin_config = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fibstarter')
+        self.source_bin_config = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fibonacci', 'templates', 'fibstarter')
+        self.target_bin_config = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fibstarter')
 
-        with open(target_bin_config, "wb") as f_conf:
-            f_conf.write(open(source_bin_config).read())
-        os.system('chmod +x %s' % target_bin_config)
+        with open(self.target_bin_config, "wb") as f_conf:
+            f_conf.write(open(self.source_bin_config).read())
+        os.system('chmod +x %s' % self.target_bin_config)
+
+    def run(self):
+        # Umcomment this line only if pip is installed properly
+        # call(["pip install -r requirements.txt --no-clean"], shell=True)
+
+        install.run(self)
+        self._create_bin()
+
         print
         print '#'*30
         print '#'*30
@@ -33,8 +41,8 @@ class Installer(install):
         print '2. Change configuration, such as HTTP server host/port and logging path.'
         print
         print 'Example Usage:'
-        print 'Start Usage: %s -c ./fib.conf' % target_bin_config
-        print 'Help Usage: %s -h' % target_bin_config
+        print 'Start Usage: %s -c ./fib.conf' % self.target_bin_config
+        print 'Help Usage: %s -h' % self.target_bin_config
         print 'Query Link: %s' % 'http://127.0.0.1:8889/fib?n=100'
         print 'Curl Link: %s' % 'curl -X GET http://127.0.0.1:8889/fib?n=-1'
         print 'ATTENTION: Please be attention to use big N for the query, as it might take a while to return the result.'
@@ -42,13 +50,6 @@ class Installer(install):
         print '#'*30
         print
 
-
-    def run(self):
-        # Umcomment this line only if pip is installed properly
-        # call(["pip install -r requirements.txt --no-clean"], shell=True)
-
-        install.run(self)
-        self._create_bin()
 
 setup(
     name=PACKAGE_NAME,
